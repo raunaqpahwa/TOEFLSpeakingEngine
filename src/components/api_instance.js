982df8cfc16c1b54833667ba8242da1e0fa40c40
Raunaq <raunaqpahwa@gmail.com>
2024-03-06 18:51:11 -0500

    Update api_instance.js

--- src/components/api_instance.js
+++ src/components/api_instance.js
@@ -4,7 +4,6 @@ const instance = axios.create({
     baseURL: 'https://api.openai.com/v1',
     headers: {
         "Content-Type": 'application/json',
-        Authorization: 'Bearer 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'
     }
 });
 
